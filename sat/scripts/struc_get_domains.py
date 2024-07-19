34996ed41e2dbe92c8c9ec6ce9ed8750d78d1f01
KarenZ2023 <karen.zhu2023@gmail.com>
2024-07-19 09:57:07 -0700

    remove format_chainsaw_domains func and change corresponding func

--- sat/scripts/struc_get_domains.py
+++ sat/scripts/struc_get_domains.py
@@ -13,75 +13,54 @@ from .utils.structure import pdb_to_structure_object, write_structure_subset
 
 def parse_chainsaw_file(chainsaw_file):
     """
-    Given text file of chainsaw results, read the results into a table and 
-    return a dictionary of structure name:domain boundary key value pairs.
-    If a structure has zero domains, it will not be included in the dictionary.
+    Given text file of chainsaw results, read the results into a table and return a nested dictionary.
+    The dictionary key is the structure name (i.e. chain_id in the chainsaw file). 
+    Each structure name maps to an inner dictionary containing key-value pairs that
+    represent the characteristics of the structure file from the chainsaw results.
 
+    Inputs:
     - chainsaw_file: chainsaw results text file, containing chain_id, sequence_md5, nres, ndom, chopping, confidence, time_sec
-    - domains_dict: dictionary of structure name: domain boundaries
+
+    Outputs:
+    - domains_dict: a nested dictionary of each structure's chainsaw result. 
+                    {stucture_name: {chain_id:value, sequence_md5:value, nres:value, ndom:value, chopping:value, confidence:value, time_sec:value}}
+                    Domain boundaries are found by using the "chopping" key. 
+                    Structures with no domains have their "chopping" key filled with '1-nres' (from one to the total number of residues).
     """
     chainsaw_table = pd.read_csv(chainsaw_file, sep='\t')
 
-    chainsaw_table['chopping'] = chainsaw_table['chopping'].fillna(value='1-nres')
+    chainsaw_table['chopping'] = chainsaw_table['chopping'].fillna(chainsaw_table['nres'].apply(lambda nres: f'1-{nres}'))
 
     domains_dict = chainsaw_table.set_index('chain_id').to_dict(orient='index')
 
     return domains_dict
 
-def parse_domain(domain_boundaries):
+def parse_domain(domain_boundary):
     """
-    Given domain_boundaries (e.g. 1-10), generate a list of consecutive numbers from the first domain residue to
-    the last domain residue.
+    Given a domain_boundary (e.g. 1-10 or 10-20_50-100), generate a list of consecutive numbers from the first domain residue to
+    the last domain residue. Discontinuous domains are separated by underscores.
 
     Inputs:
-    - domain_boundaries: boundaries from a domain or subdomain
+    - domain_boundaries: boundaries from a domain
 
     Outputs:
     - domain_residues: a list of domain residues
     """
-    domain_start = int(domain_boundaries.split('-')[0])
-    domain_end = int(domain_boundaries.split('-')[1])
-    domain_residues = [i for i in range(domain_start, domain_end+1)]
+    if "," in domain_boundary:
+        raise ValueError(f"{domain_boundary} domain boundary has a comma, which indicates more than one domain. \
+                         Only one domain is accepted (e.g. 1-10 or 10-20_50-100).")
+    
+    else:
+        domain_residues =[]
+        subdomains = domain_boundary.split('_')
+        for subdomain in subdomains:
+            subdomain_start = int(subdomain.split('-')[0])
+            subdomain_end = int(subdomain.split('-')[1])
+            subdomain_residues = [i for i in range(subdomain_start, subdomain_end+1)]
+            domain_residues.extend(subdomain_residues)
 
     return domain_residues 
 
-
-def format_chainsaw_domains(structure_name, domain_dict):    
-    """
-    Given the structure name and domain dictionary, retrieve the domain boundaries
-    for the given structure. Use the domain boundaries to create a domain_residues_list. 
-    Each list in domain_residues_list is some iterable/list of numbers, where each number is the position
-    of one of the residues to keep.
-
-    Input: 
-    - structure_name: name of the structure
-    - domain_dict: dictionary of structure name: domain boundaries
-    
-    Output:
-    - domain_residues_list: a list of lists of domain residues corresponding to the structure_name. 
-    """
-
-    domain_boundaries = domain_dict[structure_name]['chopping']
-    domain_residues_list = []
-    
-    if domain_boundaries == '1-nres':
-        domain_boundaries = '1-' + str(domain_dict[structure_name]['nres'])
-        domain_residues_list.append(parse_domain(domain_boundaries))
-        domain_list = ['FULL']
-    else:
-        domain_list = domain_boundaries.split(',')
-        for domain in domain_list:
-            subdomain_list = domain.split('_')
-            subdomain_residues_list = []
-            for subdomain in subdomain_list:
-                subdomain_residues_list = subdomain_residues_list + parse_domain(subdomain)
-            domain_residues_list.append(subdomain_residues_list)
-    
-    zipped_domains_and_residues = zip(domain_list, domain_residues_list)
-    
-    talk_to_me(f"{structure_name} has {len(domain_residues_list)} domain(s).")
-    
-    return zipped_domains_and_residues
 
 def get_pdb_filename(file_path):
     """
@@ -98,12 +77,13 @@ def get_pdb_filename(file_path):
 
 def get_outfile_name(file_path, domain_boundary):
     """
-    Given the path to a pdb file and residue numbers for the start and end of the domain,
+    Given the path to a pdb file and domain boundary of a structure,
     generate the output file name
 
     Inputs:
     - file_path: path to pdb structure file
-    - domain_boundary: a string that indicates the domain boundaries (e.g. '5-20')
+    - domain_boundary: a string that indicates the domain boundary (e.g. '10-20' or '10-20_50-100')
+      Discontinuous domains are separated by underscores.
 
     Output:
     - output_file_name: name of the extracted domain output file
@@ -114,25 +94,40 @@ def get_outfile_name(file_path, domain_boundary):
     return output_file_name
 
 
-def struc_extract_residues(pdb_file_path, zipped_domains_and_residues, min_domain_length, outfile_dir):
+def struc_extract_residues(pdb_file_path, domains_dict, min_domain_length, outfile_dir):
     """
     For the pdb file, extract and output the domains (pdb format) that meet the min_domain_length requirement.
     
     - pdb_file_path: path to pdb file
-    - zipped_domains_and_residues: a zipped object of domain boundaries and their list of residues
+    - domains_dict: a nested dictionary of each structure's chainsaw result. 
+                    {stucture_name: {chain_id:value, sequence_md5:value, nres:value, ndom:value, chopping:value, confidence:value, time_sec:value}}
     - min_domain_length: The length cutoff for the domains. 
                          If domain length < min_domain_length, the domain will not be written out to file.
     - outfile_dir: directory to output the pdb files
     """
     structure = pdb_to_structure_object(pdb_file_path, structure_name="structure")
+    structure_name = get_pdb_filename(pdb_file_path)
+    domain_boundaries = domains_dict[structure_name]['chopping'].split(",")
+    
+    talk_to_me(f"{structure_name} has {len(domain_boundaries)} domains.")
 
-    for domain_boundary, domain_residues in zipped_domains_and_residues:
+    ndom_extracted = 0
+    for domain_boundary in domain_boundaries:
+        domain_residues = parse_domain(domain_boundary)
         if len(domain_residues) < min_domain_length:
+            talk_to_me(f"{structure_name} with {domain_boundary} domain boundary does not meet the minimum domain length. This domain will not be extracted.")
             continue
         else:
-            output_file_name = get_outfile_name(pdb_file_path, domain_boundary)
+            ndom_extracted +=1
+            if domain_boundary == "1-"+ str(domains_dict[structure_name]['nres']):
+                output_file_name = get_outfile_name(pdb_file_path, domain_boundary="FULL")
+            else:
+                output_file_name = get_outfile_name(pdb_file_path, domain_boundary=domain_boundary)
+
             file_path = os.path.join(outfile_dir, output_file_name) 
             write_structure_subset(structure, residues_to_keep=domain_residues, outfile=file_path)
+
+    talk_to_me(f"{structure_name}: {ndom_extracted} domains extracted successfully")
 
 def struc_get_domains_main(args):
     domain_dict = parse_chainsaw_file(args.chainsaw_file_path)
@@ -142,10 +137,8 @@ def struc_get_domains_main(args):
         raise ValueError(f"{structure_name} is not found in the chainsaw file.")
     
     else:
-        zipped_domains_and_residues = format_chainsaw_domains(structure_name=structure_name, domain_dict=domain_dict)
         make_output_dir(args.outfile_dir, is_dir=True)
-        struc_extract_residues(args.structure_file_path, zipped_domains_and_residues, args.min_domain_length, args.outfile_dir)
-        talk_to_me(f"{structure_name} domains extracted successfully.")
+        struc_extract_residues(args.structure_file_path, domain_dict, args.min_domain_length, args.outfile_dir)
     
 
 if __name__ == "__main__":
--- tests/test_struc_get_domains.py
+++ tests/test_struc_get_domains.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import filecmp
+import pytest
 from sat.scripts.struc_get_domains import (
-    format_chainsaw_domains,
     parse_domain,
     struc_extract_residues,
     struc_get_domains_main
@@ -13,28 +13,16 @@ class Test_Parse_Domain():
         expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         assert parse_domain(domain_boundaries) == expected
 
-class Test_Format_Chainsaw_Domains():
-
-    def test_single_domain_formatting(self):
-        domain_dict = {'structure1': {'chopping':"3-8"}}
-        expected = list(zip(["3-8"],[[3, 4, 5, 6, 7, 8]]))
-        assert list(format_chainsaw_domains('structure1', domain_dict)) == expected
-
-    def test_multiple_domains_formatting(self):
-        domain_dict = {'structure1': {'chopping':"3-8,11-15"}}
-        expected = list(zip(["3-8", "11-15"] ,[[3, 4, 5, 6, 7, 8], [11, 12, 13, 14, 15]]))
-        assert list(format_chainsaw_domains('structure1', domain_dict)) == expected
-
     def test_single_domain_with_subdomain_formatting(self):
-        domain_dict = {'structure1': {'chopping':"3-8_11-15"}}
-        expected = list(zip(["3-8_11-15"] ,[[3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15]]))
-        assert list(format_chainsaw_domains('structure1', domain_dict)) == expected
+        domain_boundaries = "3-8_11-15"
+        expected = [3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15]
+        assert parse_domain(domain_boundaries) ==  expected
+    
+    def test_multiple_domains(self):
+        domain_boundaries= "3-8,11-15"
+        with pytest.raises(ValueError):
+            parse_domain(domain_boundaries)
 
-    def test_complex_domains_with_subdomains_formatting(self):
-        domain_dict = {'structure1': {'chopping':"3-8_11-15,20-25,30-32_40-45"}}
-        expected = list(zip( ["3-8_11-15", "20-25", "30-32_40-45"],
-                            [[3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15], [20, 21, 22, 23,24,25], [30,31,32,40,41,42,43,44,45]]))
-        assert list(format_chainsaw_domains('structure1', domain_dict)) == expected
 
 class Test_Struc_Extract_Residues():
 
@@ -44,15 +32,8 @@ class Test_Struc_Extract_Residues():
         pdb_file_path = (
             "tests/test_data/structure_related/get_domains_chainsaw/input_min_length/AB537968__BAJ06111.1__X__00001.pdb"
         )
-        zipped_domains_and_residues = zip(
-            ["4","4-7","4-8","50-60","7-8_15-16","60-65_70-75_80-85"],
-            [[4],
-            [4,5,6,7],
-            [4,5,6,7,8],
-            [50,51,52,53,54,55,56,57,58,59,60],
-            [7,8,15,16],
-            [60,61,62,63,64,65,70,71,72,73,74,75,80,81,82,83,84,85]]
-        )
+        domain_dict = {"AB537968__BAJ06111.1__X__00001":
+                       {'chopping':'4-7,4-8,50-60,7-8_15-16,60-65_70-75_80-85', 'nres':94}}
         min_domain_length = 5
         outfile_dir = tmp_path/"test_output"
         outfile_dir.mkdir()
@@ -60,7 +41,7 @@ class Test_Struc_Extract_Residues():
         expected_outfile_dir = Path("tests/test_data/structure_related/get_domains_chainsaw/output_min_length")
 
         # Running script
-        struc_extract_residues(pdb_file_path, zipped_domains_and_residues,  min_domain_length, outfile_dir)
+        struc_extract_residues(pdb_file_path, domain_dict,  min_domain_length, outfile_dir)
 
         # Compare observed and expected
         for observed_outfile in  outfile_dir.iterdir():
